9ec185a4c6e14eb388385fdc98f5ee83c478c9d4
Yaona <72598968+yaona807@users.noreply.github.com>
2026-09-23 19:58:14 +0900

    test(frontend): add External Solid callback data

---
+++ solid/frontend-authoring/src/items.js
@@ -0,0 +1,10 @@
+export const baseItems = Object.freeze([
+  Object.freeze({ label: 'alpha' }),
+  Object.freeze({ label: 'beta' }),
+]);
+
+export const updatedItems = Object.freeze([
+  Object.freeze({ label: 'beta' }),
+  Object.freeze({ label: 'gamma' }),
+  Object.freeze({ label: 'delta' }),
+]);
